d91dfe3c9921dc8a4dee37fa1083d9f86c984cd6
stainsleto <stainsmault@gmail.com>
2022-11-25 21:34:02 +0530

    weather app

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const apiKey = "335a8a39a3281df7c50e7f934988de31"
 const lati = 	13.067439
 const long = 	80.237617
 const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=${apiKey}`)
-const data = currentWeather.map((weath) => weath)
-const temp = data.weather.main
+const allData = currentWeather.then(response => response.json());
+const data = allData.then( (daata) => {
+    document.getElementById('temp').textContent = `${daata.main.temp}`;
+    document.getElementById('desc').textContent = `${daata.weather[0].main}`;
+    document.getElementById('name').textContent = `${daata.name}`;
+});
+console.log(allData)
 
-console.log(temp-273)
