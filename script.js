3c2d486da3227722921cda8cb301cf5a865baacf
stainsleto <stainsmault@gmail.com>
2022-11-27 22:37:11 +0530

    Weather App

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const apiKey = "335a8a39a3281df7c50e7f934988de31"
-const lati = 	13.067439
-const long = 	80.237617
-const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=${apiKey}`)
-const allData = currentWeather.then(response => response.json());
-const data = allData.then( (daata) => {
-    document.getElementById("weather-info").textContent = daata.weather[0].main;
-    const temperature = (daata.main.temp - 273).toFixed(0)+ '°C';
+function getWeather(){
+    const apiKey = "335a8a39a3281df7c50e7f934988de31";
+    
+    // Geo API  ------------------------ Begins ------
 
-    document.getElementById("temperature").textContent = temperature;
-    const mb = " mb";
-    document.getElementById("pressure").textContent = daata.main.pressure + mb;
-    const per = " %";
-    document.getElementById("humidity").textContent = daata.main.humidity + per;
-    const km = " Km";
-    document.getElementById("visiblity").textContent = daata.visibility/1000 +km ;
-    document.getElementById("city").textContent = daata.name;
-    document.getElementById("wind-speed").textContent = (daata.wind.speed) + " Km/h" ;
-    document.getElementById("temp-min").textContent = (daata.main.temp_min - 273).toFixed(0) + "°C";
-    document.getElementById("temp-max").textContent = (daata.main.temp_max - 273).toFixed(0) + "°C";
-});
-console.log(allData)
+    const city = document.getElementById('city-input').value;
+    console.log(city)
+    const country = "India"; 
+    const geo = fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=5&appid=${apiKey}`);
+    const geoData = geo.then( (response) => response.json());
+    const lati = geoData.then((data) => data[0].lon);
+    const longi = geoData.then( (data) => data[0].lat);
+    console.log(lati)
+    
+     // Geo API  ------------------------ Ends ----------- 
 
+    const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=${apiKey}`)
+    const allData = currentWeather.then(response => response.json());
+    
+    const data = allData.then( (daata) => {
+        document.getElementById("weather-info").textContent = daata.weather[0].main;
+        const temperature = (daata.main.temp - 273).toFixed(0)+ '°C';
+
+        document.getElementById("temperature").textContent = temperature;
+        const mb = " mb";
+        document.getElementById("pressure").textContent = daata.main.pressure + mb;
+        const per = " %";
+        document.getElementById("humidity").textContent = daata.main.humidity + per;
+        const km = " Km";
+        document.getElementById("visiblity").textContent = daata.visibility/1000 +km ;
+        document.getElementById("city").textContent = daata.name;
+        document.getElementById("wind-speed-value").textContent = (daata.wind.speed) + " Km/h" ;
+        document.getElementById("temp-min-value").textContent = (daata.main.temp_min - 273).toFixed(0) + "°C";
+        document.getElementById("temp-max-value").textContent = (daata.main.temp_max - 273).toFixed(0) + "°C";
+    });
+
+    console.log(allData)
+
+
+}
