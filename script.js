a9c3ad45ea4637c035a9b17479f6f60180a39148
stainsleto <stainsmault@gmail.com>
2022-12-07 20:14:31 +0530

    checking mobile button

--- script.js
+++ script.js
@@ -8,8 +8,35 @@
 // }
 const button = document.getElementById('search-icon')
 
+// button
+
 button.addEventListener('touchstart',() => {
-    return getWeather()
+    const apiKey = "b13b3dd8053e9d7f1016053308bd0eff";
+    
+
+    const city = document.getElementById('city-input').value;  // city Info
+    
+
+    const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    const allData = currentWeather.then(response => response.json());
+    
+    const data = allData.then( (daata) => {
+        document.getElementById("weather-info").textContent = daata.weather[0].main;
+        const temperature = (daata.main.temp - 273).toFixed(0)+ '°C';
+        document.getElementById("temperature").textContent = temperature;
+        const mb = " mb";
+        document.getElementById("pressure").textContent = daata.main.pressure + mb;
+        const per = " %";
+        document.getElementById("humidity").textContent = daata.main.humidity + per;
+        const km = " Km";
+        document.getElementById("visiblity").textContent = daata.visibility/1000 +km ;
+        document.getElementById("city").textContent = daata.name;
+        document.getElementById("weather-description").textContent = daata.weather[0].description;
+        document.getElementById("wind-speed-value").textContent = (daata.wind.speed) + " Km/h" ;
+        document.getElementById("temp-min-value").textContent = (daata.main.temp_min - 273).toFixed(0) + "°C";
+        document.getElementById("temp-max-value").textContent = (daata.main.temp_max - 273).toFixed(0) + "°C";
+
+    });
 })
 
 
