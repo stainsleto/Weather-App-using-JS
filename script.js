2830e6c524f927017980e1b50f17f2dfa905f084
stainsleto <stainsmault@gmail.com>
2022-12-07 20:59:05 +0530

    checking mobile button

--- script.js
+++ script.js
@@ -6,46 +6,11 @@
 // else{
 //     hero.show()
 // }
-const button = document.getElementById('search-icon')
-
-// button
-
-button.addEventListener('touchstart',() => {
-    const apiKey = "b13b3dd8053e9d7f1016053308bd0eff";
-    
-
-    const city = document.getElementById('city-input').value;  // city Info
-    
-
-    const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    const allData = currentWeather.then(response => response.json());
-    
-    const data = allData.then( (daata) => {
-        document.getElementById("weather-info").textContent = daata.weather[0].main;
-        const temperature = (daata.main.temp - 273).toFixed(0)+ '°C';
-        document.getElementById("temperature").textContent = temperature;
-        const mb = " mb";
-        document.getElementById("pressure").textContent = daata.main.pressure + mb;
-        const per = " %";
-        document.getElementById("humidity").textContent = daata.main.humidity + per;
-        const km = " Km";
-        document.getElementById("visiblity").textContent = daata.visibility/1000 +km ;
-        document.getElementById("city").textContent = daata.name;
-        document.getElementById("weather-description").textContent = daata.weather[0].description;
-        document.getElementById("wind-speed-value").textContent = (daata.wind.speed) + " Km/h" ;
-        document.getElementById("temp-min-value").textContent = (daata.main.temp_min - 273).toFixed(0) + "°C";
-        document.getElementById("temp-max-value").textContent = (daata.main.temp_max - 273).toFixed(0) + "°C";
-
-    });
-})
 
 
 function getWeather(){
     const apiKey = "b13b3dd8053e9d7f1016053308bd0eff";
-    
-
     const city = document.getElementById('city-input').value;  // city Info
-    
 
     const currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     const allData = currentWeather.then(response => response.json());
@@ -65,7 +30,6 @@ function getWeather(){
         document.getElementById("wind-speed-value").textContent = (daata.wind.speed) + " Km/h" ;
         document.getElementById("temp-min-value").textContent = (daata.main.temp_min - 273).toFixed(0) + "°C";
         document.getElementById("temp-max-value").textContent = (daata.main.temp_max - 273).toFixed(0) + "°C";
-
     });
 
     
